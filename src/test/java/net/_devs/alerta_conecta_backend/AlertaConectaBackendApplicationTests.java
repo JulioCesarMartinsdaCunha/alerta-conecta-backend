95c843c43a1f629669926975ee7c4c05eb070608
julio cesar <julio cesar>
2025-10-07 19:42:18 -0300

    Atualizando arquivos iniciais. (Zero atualizações significativas)

---
+++ src/test/java/net/_devs/alerta_conecta_backend/AlertaConectaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net._devs.alerta_conecta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaConectaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
